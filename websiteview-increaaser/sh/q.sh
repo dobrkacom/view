3388d001239f6399ade3ceeefcef85d439534bc6
dobrkacom <ithpe.co@gmail.com>
2024-09-26 09:48:00 +0330

    Update q.sh

--- websiteview-increaaser/sh/q.sh
+++ websiteview-increaaser/sh/q.sh
@@ -1,13 +1,11 @@
-
 q="https://www.google.com/search?q=%D8%B3%D8%B1%D9%88%D8%B1+g9+site%3A+www.dobrka.com"
 q1="https://dobrka.com/10470-%D8%B3%D8%B1%D9%88%D8%B1-hp-g9"
 input="PROXY-List/http.txt"
 while IFS= read -r line
-do 
-  export http_proxy="http://$line";
-  wget  --timeout=5 --header="Accept: text/html" --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" $q
-   wget  --timeout=5 --header="Accept: text/html" --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" $q1
-   rm *g9*;
-   rm *html*;
+do
+  chromium-browser --proxy-server="$line" $q 
+  chromium-browser --proxy-server="$line" $q1
+xdotool key Alt+F4;
+
 done < "$input"
 ./q.sh
